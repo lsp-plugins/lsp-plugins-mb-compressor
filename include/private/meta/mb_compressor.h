be100968340eeb73ce32d140f9713154ebbfb6d3
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-08-03 21:26:22 +0300

    Initial import

---
+++ include/private/meta/mb_compressor.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2021 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-mb-compressor
+ * Created on: 3 авг. 2021 г.
+ *
+ * lsp-plugins-mb-compressor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-mb-compressor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-mb-compressor. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRIVATE_META_MB_COMPRESSOR_H_
+#define PRIVATE_META_MB_COMPRESSOR_H_
+
+#include <lsp-plug.in/plug-fw/meta/types.h>
+#include <lsp-plug.in/plug-fw/const.h>
+
+
+namespace lsp
+{
+    namespace meta
+    {
+        struct mb_compressor_metadata
+        {
+        };
+    } // namespace meta
+} // namespace lsp
+
+
+#endif /* PRIVATE_META_MB_COMPRESSOR_H_ */
