5fc885f6321ee669adaec9c281215359aa28913e
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-03 19:48:50 +0300

    Updated build scripts and dependencies

--- project.mk
+++ project.mk
@@ -24,6 +24,7 @@ ARTIFACT_NAME               = lsp-plugins-mb-compressor
 ARTIFACT_TYPE               = plug
 ARTIFACT_DESC               = LSP Multiband Compressor Plugin Series
 ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_HEADERS     = 0
 ARTIFACT_VERSION            = 1.0.8-devel
 
 
