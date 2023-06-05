f9747c5360edc92a9b0b0bfcd8cf67d9b973a1ec
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-06-05 16:54:48 +0300

    Updated documentation

--- src/doc/manuals/plugins/mb_compressor.php
+++ src/doc/manuals/plugins/mb_compressor.php
@@ -48,6 +48,12 @@
 	sidechain signal. 
 	</li>
 </ul>
+<p>
+	The compressor can provide two release times. If the compressor's envelope indicated by the dot on the
+	compression curve is above the ‘Release Thresh’ then the ‘Release Time’ parameter is taken as a release time
+	for processing. Otherwise, if the envelope is below the ‘Release Thresh’ then the ‘Attack Time’ parameter is
+	taken as a release time for processing.
+</p>
 <p><b>Controls:</b></p>
 <ul>
 	<li>
